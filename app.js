64eeaa402c66cbab7eeb2bbd214383808d8f70b6
Nithin Aashik <nithinprince2000@gmail.com>
2020-08-12 14:49:26 +0530

    socket update

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
 
-const gameSocket = require("./routes/gameSockets");
-
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 app.get("/game", (req, res) => {
     res.render("index");
 });
-
-// app.use("/Socket", gameSocket);
 
 module.exports = app;
